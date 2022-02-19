94f04164410c24baed26ec2ffa17b561dff71e85
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2022-02-19 18:06:18 +0800

    updated annotations and some variables because some did not match causing the code to not work

--- qr_reader.py
+++ qr_reader.py
@@ -2,49 +2,49 @@
 Assignment 10
  Contact Tracing App
  Create a python program that will read QRCode using your webcam
-	- You may use any online QRCode generator to create QRCode
-	- All personal data are in QRCode 
-	- You may decide which personal data to include
-	- All data read from QRCode should be stored in a text file 
-   - including the date and time it was readimport cv2
+	> You may use any online QRCode generator to create QRCode
+	> All personal data are in QRCode 
+	> You may decide which personal data to include
+	> All data read from QRCode should be stored in a text file 
+    > including the date and time it was readimport cv2
 '''
 
 import cv2
 import pyzbar.pyzbar as pyzbar
 import datetime
 
-# function to read QR Code
+# defined function to interpret the qr code displayed and scanned by the system
 def readQRc(frame):
-    # decoding the QR code from webcam
+    # deciphers the displayed qr code scanned from the webcam
     qr_code = pyzbar.decode(frame)
     for qrcode in qr_code:
         x, y , w, h = qrcode.rect
 
-        # the date and time configuration
+        # configures the date and time
         current = datetime.datetime.current()
-        datecurrent = "%s/%s/%s" % (current.daycurrent.month,current.year)
-        timecurrent = "%s:%s" % (current.minute,current.hour)
+        current_date = "%s/%s/%s" % (current.daycurrent.month,current.year)
+        current_time = "%s:%s" % (current.minute,current.hour)
 
-        # drawing a rectangle to see if code is detected
-        qrcodeInfo = qrcode.data.decode('utf-8')
+        # displays a rectangle to detect the qr code
+        infoqrcode = qrcode.data.decode('utf-8')
         cv2.rectangle(frame, (x, y),(x+w, y+h), (255, 0, 0), 2)
         
-        # text to show decoded data
-        font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-        cv2.putText(frame, "CONTACT TRACING INFORMATION", (x + 6, y - 6), font, 1.0, (0, 0, 255), 3)
+        # displays the data
+        font_type = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+        cv2.putText(frame, "CONTACT TRACING INFORMATION", (x + 6, y - 6), font_type, 1.0, (0, 0, 255), 3)
 
-        # writing it into a text file
+        # inputs into a text format
         with open("qr_deets.txt", mode ='w') as file:
-            file.write("Scanned QR Code:" + qrcodeInfo + (f"\n\n\nDate: {datecurrent}\nTime: {timecurrent}"))
+            file.write("Scanned QR Code:" + infoqrcode + (f"\n\n\nDate: {current_date}\nTime: {current_time}"))
     return frame
 
 def read():
 
-    # initializing web cam 
+    # initiates a web cam to scan qr
     webcam = cv2.VideoCapture(0)
     ret, frame = webcam.read()
 
-    # loop to continuously scan for QR and not close
+    # in a continuous loop to scan the intended qr
     while ret:
         ret, frame = webcam.read()
         frame = readQRc(frame)
