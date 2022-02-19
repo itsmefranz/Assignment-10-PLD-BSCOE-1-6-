220e493a121d4761e6d6ca123ed9b30b8080d158
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2022-02-19 16:20:09 +0800

    defined readQRc function along with its corresponding subfunctions and added annotations

--- qr_reader.py
+++ qr_reader.py
@@ -13,3 +13,28 @@ import cv2
 import pyzbar.pyzbar as pyzbar
 import datetime
 
+# function to read QR Code
+def readQRc(frame):
+    # decoding the QR code from webcam
+    qr_code = pyzbar.decode(frame)
+    for qrcode in qr_code:
+        x, y , w, h = qrcode.rect
+
+        # the date and time configuration
+        current = datetime.datetime.current()
+        datecurrent = "%s/%s/%s" % (current.daycurrent.month,current.year)
+        timecurrent = "%s:%s" % (current.minute,current.hour)
+
+        # drawing a rectangle to see if code is detected
+        qrcodeInfo = qrcode.data.decode('utf-8')
+        cv2.rectangle(frame, (x, y),(x+w, y+h), (255, 0, 0), 2)
+        
+        # text to show decoded data
+        font = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
+        cv2.putText(frame, "CONTACT TRACING INFORMATION", (x + 6, y - 6), font, 1.0, (0, 0, 255), 3)
+
+        # writing it into a text file
+        with open("qr_deets.txt", mode ='w') as file:
+            file.write("Scanned QR Code:" + qrcodeInfo + (f"\n\n\nDate: {datecurrent}\nTime: {timecurrent}"))
+    return frame
+
